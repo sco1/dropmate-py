a8f4ce6707809bc316c6295f23e8ff0fede64034
S. Co1 <sco1.git@gmail.com>
2023-08-05 19:44:28 -0400

    Update column parsing for unified header names
    Starting with app version 1.5.16 the iOS and Android apps should both output the same header information

--- dropmate_py/parser.py
+++ dropmate_py/parser.py
@@ -12,6 +12,9 @@ class ColumnIndices:
     Attribute names are assumed to correspond to the Dropmate log header name. Columns contained may
     vary by Dropmate app version, so if they are not present in the compiled log they will remain at
     a value of `-1`, indicating they were not found.
+
+    Current header names are assumed to be as of Dropmate app version 1.5.16, and should be unified
+    across the iOS and Android app outputs (this isn't the case for older app versions).
     """
 
     serial_number: int = -1
@@ -19,12 +22,21 @@ class ColumnIndices:
     battery: int = -1
     device_health: int = -1
     firmware_version: int = -1
+    log_timestamp: int = -1
+    log_altitude: int = -1
+    total_flights: int = -1
+    flights_over_18kft: int = -1
+    recorded_flights: int = -1
+    flight_index: int = -1
     start_time_utc: int = -1
     end_time_utc: int = -1
     start_barometric_altitude_msl_ft: int = -1
     end_barometric_altitude_msl_ft: int = -1
-    dropmate_internal_time: int = -1
-    last_scanned_time: int = -1
+    dropmate_internal_time_utc: int = -1
+    last_scanned_time_utc: int = -1
+    scan_device_type: int = -1
+    scan_device_os: int = -1
+    dropmate_app_version: int = -1
 
     def __iter__(self) -> t.Generator[str, None, None]:
         for f in fields(self):
--- tests/test_heading_parse.py
+++ tests/test_heading_parse.py
@@ -1,6 +1,6 @@
 from dropmate_py.parser import ColumnIndices
 
-SAMPLE_FULL_HEADER = "serial_number,uid,battery,device_health, firmware_version,log_timestamp,log_altitude,total_flights,flights_over_18kft,recorded_flights,flight_index,start_time_utc,end_time_utc,start_barometric_altitude_msl_ft,end_barometric_altitude_msl_ft,dropmate_internal_time,last_scanned_time"
+SAMPLE_FULL_HEADER = "serial_number,uid,battery,device_health,firmware_version,log_timestamp,log_altitude,total_flights,flights_over_18kft,recorded_flights,flight_index,start_time_utc,end_time_utc,start_barometric_altitude_msl_ft,end_barometric_altitude_msl_ft,dropmate_internal_time_utc,last_scanned_time_utc,scan_device_type,scan_device_os,dropmate_app_version"
 
 
 def test_parse_indices_full_header() -> None:
@@ -10,12 +10,21 @@ def test_parse_indices_full_header() -> None:
         battery=2,
         device_health=3,
         firmware_version=4,
+        log_timestamp=5,
+        log_altitude=6,
+        total_flights=7,
+        flights_over_18kft=8,
+        recorded_flights=9,
+        flight_index=10,
         start_time_utc=11,
         end_time_utc=12,
         start_barometric_altitude_msl_ft=13,
         end_barometric_altitude_msl_ft=14,
-        dropmate_internal_time=15,
-        last_scanned_time=16,
+        dropmate_internal_time_utc=15,
+        last_scanned_time_utc=16,
+        scan_device_type=17,
+        scan_device_os=18,
+        dropmate_app_version=19,
     )
 
     indices = ColumnIndices.from_header(SAMPLE_FULL_HEADER)
@@ -32,12 +41,21 @@ def test_parse_indices_old_header() -> None:
         battery=2,
         device_health=-1,
         firmware_version=-1,
+        log_timestamp=3,
+        log_altitude=4,
+        total_flights=5,
+        flights_over_18kft=7,
+        recorded_flights=8,
+        flight_index=9,
         start_time_utc=10,
         end_time_utc=11,
         start_barometric_altitude_msl_ft=12,
         end_barometric_altitude_msl_ft=13,
-        dropmate_internal_time=-1,
-        last_scanned_time=-1,
+        dropmate_internal_time_utc=-1,
+        last_scanned_time_utc=-1,
+        scan_device_type=-1,
+        scan_device_os=-1,
+        dropmate_app_version=-1,
     )
 
     indices = ColumnIndices.from_header(SAMPLE_OLD_HEADER)
